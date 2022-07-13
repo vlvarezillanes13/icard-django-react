7a808126a096078be9eb4cdf7686894ce06f8027
Vicente Álvarez <v.lvarez.illanes@gmail.com>
2022-07-12 21:29:45 -0400

    modificar post y update password & propia apiview usuario logeado

---
+++ icard_django/icard/users/api/router.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from users.api.views import UserApiViewSet, UserView
+
+router_user = DefaultRouter()
+
+router_user.register(
+    prefix='users', basename='users', viewset=UserApiViewSet
+)
+
+urlpatterns = [
+    path('auth/me/', UserView.as_view())
+]
